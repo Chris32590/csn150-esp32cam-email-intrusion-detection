c98506947a6efed21050971ffc0d69d196e5050c
Christopher <Christopher.perez30@aol.com>
2025-12-17 14:43:39 -0500

    Create ESP32CAM_Email_Alert.ino with config settings
    Add ESP32CAM_Email_Alert.ino with WiFi and author password definitions

---
+++ screenshots/code/ESP32CAM_Email_Alert.ino
@@ -0,0 +1,5 @@
+Where it says wifi name and password you use your own information
+
+#define WIFI_SSID "YOUR_WIFI_NAME"
+#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
+#define AUTHOR_PASS "YOUR_APP_PASSWORD"
